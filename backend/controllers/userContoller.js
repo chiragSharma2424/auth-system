88c87dec778022f20fd4d8e2a31ed3e191767151
chirag <sharmachirag242004@gmail.com>
2025-06-28 23:28:13 +0530

    cookie added

--- backend/controllers/userContoller.js
+++ backend/controllers/userContoller.js
@@ -33,7 +33,14 @@ export const register = async (req, res) => {
         await user.save();
 
         // generate a token using jwt
-        const token = jwt.sign()
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expires: '7d'});
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 
+            'none': 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
 
     } catch(err) {
         console.log(err);
