6f9e2643f3f7b651f1e3925cceff611346bb185c
chirag <sharmachirag242004@gmail.com>
2025-06-30 12:31:28 +0530

    middleware and verification routes done

--- backend/middleware/userAuth.js
+++ backend/middleware/userAuth.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const userAuth = async (req, res) => {
+export const userAuth = async (req, res, next) => {
     const { token } = req.cookies;
 
     if(!token) {
@@ -13,8 +13,22 @@ export const userAuth = async (req, res) => {
     }
 
     try {
-        jwt.verify(token, process.env.JWT_SECRET)
-    } catch(err) {
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
 
+        if(tokenDecode.id) {
+            req.body.userId = tokenDecode.id
+        } else {
+            return res.json({
+                success: false,
+                message: "not authorized login again"
+            })
+        }
+
+        next();
+    } catch(err) {
+        res.json({
+            success: false,
+            message: err.message,
+        })
     }
 }
