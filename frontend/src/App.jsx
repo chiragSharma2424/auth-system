464960bd164639021aa1e84753b68e46ce0e2dad
chirag <sharmachirag242004@gmail.com>
2025-07-01 19:10:28 +0530

    navbar done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import EmailVerify from "./pages/EmailVerify";
+import ResetPassword from "./pages/ResetPassword";
 
 function App() {
 
   return (
-   <div className="w-full bg-amber-300">
-    chirag
+   <div className="">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/email-verify" element={<EmailVerify />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+    </Routes>
    </div>
   )
 }
